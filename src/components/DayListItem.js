bea426063a26f6974d729fb46cfd160a2cdd8ff0
David Folkins <david.folkins88@gmail.com>
2020-10-26 15:59:08 -0400

    Add daylistitems and css

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
 
+const formatSpots = function(spots) {
+  if (spots <= 0) {
+    return "no spots remaining"
+  } else if (spots === 1) {
+    return "1 spot remaining" 
+  } else {
+    return `${spots} spots remaining`
+  }
+}
 export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  })
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
