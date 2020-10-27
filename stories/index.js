ab56aee64c76737086dcfecdc0226e874a390e9e
David Folkins <david.folkins88@gmail.com>
2020-10-27 14:49:21 -0400

    Small refactor

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
-import Appointment from "components/Appointment/index"
+import Appointment from "components/Appointment"
 
 storiesOf("Button", module)
   .addParameters({
