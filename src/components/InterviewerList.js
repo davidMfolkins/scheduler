890224cdee21c7adee20ff549d744a370d502f27
David Folkins <david.folkins88@gmail.com>
2020-10-27 14:16:17 -0400

    Finish interviewerlist

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem"
+
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+
+    return (
+      <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={props.interviewer===interviewer.id}
+      setInterviewer={props.setInterviewer}
+      />
+    )
+  })
+  return (
+      <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewers}
+        </ul>
+    </section>
+  )
+}
+
+
