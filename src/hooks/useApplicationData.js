d523f3d313c4b33d6527e3b4596ef5443d05ea26
David Folkins <david.folkins88@gmail.com>
2020-11-03 12:31:54 -0500

    Change application test to use await

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -72,12 +72,12 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
-
+  
   useEffect(() => {
     Promise.all([
-      axios.get('api/days'),
-      axios.get('api/appointments'),
-      axios.get('api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
       .then((all) => {
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
